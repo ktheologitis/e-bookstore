a5416d73f391818d6d969d9c4734d30b5cf95811
Konstantinos Theologitis <ktheologitis@gmail.com>
2022-08-20 21:20:17 +0200

    Added illustration in landing page

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import BasketPage from "./pages/BasketPage/BasketPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import ProductsPage from "./pages/ProductsPage/ProductsPage";
 
 const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/basket" element={<BasketPage />} />
       </Route>
