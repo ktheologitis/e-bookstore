1a9fc41b154c1fb9c881bbf9ae59f3f6b7224e8f
Konstantinos Theologitis <ktheologitis@gmail.com>
2022-08-20 18:18:45 +0200

    Added basket product card component

--- src/pages/BasketPage/BasketPage.tsx
+++ src/pages/BasketPage/BasketPage.tsx
@@ -1,5 +1,19 @@
+import BasketProductCard from "../../components/BasketProductCard/BasketProductCard";
+import PageHeader from "../../components/PageHeader/PageHeader";
+import "../../stylesheets/page.scss";
+
 const BasketPage = () => {
-  return <h1>Basket</h1>;
+  return (
+    <main className="page">
+      <PageHeader title="My Basket" />
+      <BasketProductCard
+        title="Book 1"
+        amount_selected={1}
+        price={25.55}
+        stock={3}
+      />
+    </main>
+  );
 };
 
 export default BasketPage;
