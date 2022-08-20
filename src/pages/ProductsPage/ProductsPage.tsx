17c1d9629c71abd3d0a486b5b2c535d361960cf7
Konstantinos Theologitis <ktheologitis@gmail.com>
2022-08-20 16:55:24 +0200

    Added product component. And some global css on for fonts and colors.

--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -1,11 +1,27 @@
 import ProductCard from "../../components/ProductCard/ProductCard";
+import "./product-page.scss";
 
 const ProductsPage = () => {
   return (
-    <>
-      <h1>Products</h1>;
-      <ProductCard title="Book 1" price="20" stock="2" img="" />
-    </>
+    <main className="product-page">
+      <header className="product-page__header">
+        <p>Products</p>
+      </header>
+      <main className="product-page__products">
+        <ProductCard
+          title="Book 1"
+          price="20"
+          stock={2}
+          img="https://picsum.photos/200?random=1"
+        />
+        <ProductCard
+          title="Book 1"
+          price="20"
+          stock={2}
+          img="https://picsum.photos/200?random=1"
+        />
+      </main>
+    </main>
   );
 };
 
